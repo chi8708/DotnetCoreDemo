93a4f2d49e5f472b31b4b744f5462578c311e7ef
chi8708 <983143462@qq.com>
2019-04-01 15:39:26 +0800

    7.0语法

---
+++ BasicConsole/6.0Demo.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;//导入静态类
+
+namespace BasicConsole
+{
+    //vs2015 支持
+    public class _6
+    {
+        public static void Run()
+        {
+            AutoPro();
+            ImportStaticClass();
+            NullCheck();
+            DicInit();
+           // ExceptionWhen();
+            Print();
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 自动属性
+        /// </summary>
+        public static void AutoPro()
+        {
+            Peole p = new Peole();
+            p.Name = "rose";
+            Console.WriteLine($"Name:{p.Name},Age:{p.Age}");
+
+        }
+
+        /// <summary>
+        /// 导入静态变量
+        /// </summary>
+        public static void ImportStaticClass()
+        {
+            // Console.WriteLine($"之前的使用方式: {Math.Pow(4, 2)}");
+            Console.WriteLine($"导入静态Math使用方式: {Pow(4, 2)}");
+        }
+
+        /// <summary>
+        /// null值判断
+        /// </summary>
+        public static void NullCheck()
+        {
+            int? iValue = 10;
+            Console.WriteLine(iValue?.ToString());//不需要判断是否为空
+            string name = null;
+            Console.WriteLine(name?.ToString()); // 程序不会报错，也不会输出任何值
+            Console.WriteLine("空值运算符");
+
+             List<string> list= null;
+             Console.WriteLine($"空值运算符list:{(list?.Count())??0}");
+        }
+
+        /// <summary>
+        /// 字典初始化
+        /// </summary>
+        public static void DicInit()
+        {
+            //o
+            Dictionary<int, string> dic1 = new Dictionary<int, string> { { 1, "jack" }, { 2, "rose" } };
+            foreach (var item in dic1)
+            {
+                Console.WriteLine($"key:{item.Key}value:{item.Value}");
+            }
+
+            //n
+            Dictionary<int, string> dic2 = new Dictionary<int, string> { [3] = "jack", [4] = "rose" };
+            foreach (var item in dic2)
+            {
+                Console.WriteLine($"key:{item.Key}value:{item.Value}");
+            }
+        }
+
+        //异常处理when
+        public static void ExceptionWhen()
+        {
+
+            try
+            {
+                Int32.Parse("we");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            int exceptionValue = 10;
+            try
+            {
+                Int32.Parse("we");
+            }
+            catch (Exception e) when (exceptionValue > 10)//满足条件才进入catch
+            {
+                Console.WriteLine("catch");
+            }
+
+        }
+
+        //lambda函数体，无返回值
+        public static void  Print() => Console.WriteLine("lambda函数体，无返回值");
+
+
+        //lambda函数体，返回对象
+        public static Peole GetP() => new Peole(); 
+   
+    }
+
+    public class Peole
+    {
+        public Peole() { }
+        // 7.0Expression-bodied constructor
+        public Peole(string name) => this.Name = name;
+
+        private string _email;
+        public string Name { get; set; } = "Jack";//属性初始化赋值
+
+        public int Age { get; set; } = 12;
+
+        public string Email { get => _email; set => _email = value ?? "99@qq.com"; }
+
+        //7.0解析函数
+        public void Deconstruct(out string name, out int age)
+        {
+            name = Name;
+            age = Age;
+        }
+    }
+
+}
---
+++ BasicConsole/7.0Demo.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsole
+{
+    public class _7
+    {
+        public static void Run()
+        {
+            OutVar();
+            Tuple();
+            var list= GetCharList("hello");
+            PrintStars(11);
+            Console.Read();
+        }
+
+        //out变量 out var result
+        public static void OutVar()
+        {
+            var input = "1";
+            if (int.TryParse(input, out var result))
+            {
+                Console.WriteLine("您输入的数字是：{0}", result);
+            }
+
+            int num;
+            string s ="2";
+            if (int.TryParse(s, out num))
+            {
+                Console.WriteLine("您输入的数字是：{0}", num);
+            }
+
+        }
+
+       // Install-Package System.ValueTuple
+        //tuple 元组
+        public static void Tuple()
+        {
+            var tuple1 = (1, 2);           // 使用语法糖创建元组
+            var tuple2 = ValueTuple.Create(1, 2); // 使用静态方法【Create】创建元组
+            var tuple3 = new ValueTuple<int, int>(1, 2);// 使用 new 运算符创建元组
+            Console.WriteLine($"first：{tuple1.Item1}, second：{tuple2.Item2}, 上面三种方式都是等价的。");
+
+
+            // 左边指定字段名称
+            (int one, int two) tuple4 = (1, 2);
+            Console.WriteLine($"first：{tuple4.one}, second：{tuple4.two}");
+
+            // 右边指定字段名称
+            var tuple5 = (one: 1, two: 2);
+            Console.WriteLine($"first：{tuple5.one}, second：{tuple5.two}");
+
+            // 左右两边同时指定字段名称
+            (int one, int two) tuple6 = (first: 1, second: 2);   
+            /* 此处会有警告：由于目标类型（xx）已指定了其它名称，因为忽略元组名称xxx */
+            Console.WriteLine($"first：{tuple6.one}, second：{tuple6.two}");
+
+            //解构元组
+            var (Name, Age) = new Peole();
+            Console.WriteLine($"Name:{Name},Age:{Age}");
+
+
+        }
+
+        //局部函数
+       public static IEnumerable<char> GetCharList(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                throw new ArgumentNullException(nameof(str));
+
+            return GetList();
+
+            IEnumerable<char> GetList()
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    yield return str[i];
+                }
+            }
+            //            使用方法：
+
+            //[数据类型, void] 方法名（[参数]）
+            //  {
+            //     // Method body；[] 里面都是可选项
+            //  }
+        }
+
+        /// <summary>
+        /// 具有模式的 IS 表达式
+        /// </summary>
+        /// <param name="o"></param>
+        public static void PrintStars(object o)
+        {
+            if (o is null) return;     // 常量模式 "null"
+            if (!(o is int i)) return; // 类型模式 定义了一个变量 "int i" i的值就是o的值 相当于is类型判断
+            Console.WriteLine($"i={i}");
+
+            //如果数字太长，可以按照一定的位数用“_”进行分割
+            long big = 100_000_000;
+             Console.WriteLine($"big={big}");
+        }
+    }
+}
